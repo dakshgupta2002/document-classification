f3c920ad9b9b57fa3ad7f2bae507b262fba49396
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-11-24 13:21:04 +0530

    login page and routing

--- app_ui/src/App.js
+++ app_ui/src/App.js
@@ -1,10 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './pages/Login';
+
+
 function App() {
+
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
