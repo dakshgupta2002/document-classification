6c90888e6fada386d975372d542a8d1ff8f7f570
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-11-24 21:45:23 +0530

    remove next directory

--- cloud-nine/lib/url.ts
+++
@@ -1,8 +0,0 @@
-const url = {
-    client:
-        process.env.NODE_ENV === "production"
-            ? "https://coderecs.vercel.app"
-            : "http://localhost:3000",
-    server: "",
-};
-export { url };
--- cloud-nine/screens/landing.tsx
+++
@@ -1,9 +0,0 @@
-import { Base } from "../components/landing/templates/Base";
-
-function Landing() {
-    return (
-        <Base />
-    );
-}
-
-export default Landing;
