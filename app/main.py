db7beab0cd0a3d127a27a5aedd93dd1954b18985
Piyush <piyushb544@gmail.com>
2022-11-26 12:05:32 +0530

    added cors

--- app/main.py
+++ app/main.py
@@ -2,15 +2,38 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 from script import extract_from_image, extract_from_pdf, classify_image, classify_pdf
+from fastapi.middleware.cors import CORSMiddleware
+
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
+    "*"
+]
+
 
 class Image(BaseModel):
     filename: str
     b64: str
+
+
 class Pdf(BaseModel):
     filename: str
     b64: str
 
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def index():
@@ -25,20 +48,22 @@ def index():
         "Thank you": "for using this service"
     }
 
+
 @app.post("/extract/image")
 async def index(image: Image):
     return {"text": extract_from_image(image.b64, image.filename)}
-     
+
+
 @app.post("/extract/pdf")
 async def index(pdf: Pdf):
     return {"text": extract_from_pdf(pdf.b64, pdf.filename)}
-     
+
+
 @app.post("/classify/image")
 async def index(image: Image):
     return {"class": classify_image(image.b64, image.filename)}
-    
+
+
 @app.post("/classify/pdf")
 async def index(pdf: Pdf):
     return {"text": classify_pdf(pdf.b64, pdf.filename)}
-    
-    
--- app/script.py
+++ app/script.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import pypdfium2 as pdfium
 import base64
 import pypdfium2 as pdfium
-import pickle 
+import pickle
 
 pickled_model = pickle.load(open('model.pkl', 'rb'))
 
@@ -15,54 +15,58 @@ pytesseract.tesseract_cmd = path_to_tesseract
 
 def extract_from_image(b64, filename):
     res = base64.b64decode(b64)
-    text=""
-    
+    text = ""
+
     try:
         result = open('res.jpg', 'wb')
         result.write(res)
         result.close()
-        
+
         img = Image.open('res.jpg')
         text += pytesseract.image_to_string(img)
     except:
-        print(error)
-        
+        print("error")
+
     return text
+
 
 def extract_from_pdf(b64, filename):
     print("pdf extract")
     res = base64.b64decode(b64)
-    text=""
-    
+    text = ""
+
     try:
         result = open('res.pdf', 'wb')
         result.write(res)
         result.close()
-        
+
         pdf = pdfium.PdfDocument('res.pdf')
         for page_number in range(len(pdf)):
             page = pdf.get_page(page_number)
-            pil_image = page.render_topil(scale=1, rotation=0, crop=(0, 0, 0, 0), greyscale=False, optimise_mode=pdfium.OptimiseMode.NONE)
+            pil_image = page.render_topil(scale=1, rotation=0, crop=(
+                0, 0, 0, 0), greyscale=False, optimise_mode=pdfium.OptimiseMode.NONE)
             pil_image.save("res.jpg")
             img = Image.open("res.jpg")
             text += pytesseract.image_to_string(img)
     except:
-        print(error)
-        
+        print("error")
+
     return text
+
 
 def classify_image(b64, filename):
     text = extract_from_image(b64, filename)
     res = pickled_model.predict([text])
     conf = max(pickled_model.decision_function([text])[0])
-    # if(conf <= 0):
-    #     return 'Unknown'
-    return (res[0], conf)
-    
+    if(conf <= 0):
+        return 'unknown'
+    return res[0]
+
+
 def classify_pdf(b64, filename):
     text = extract_from_pdf(b64, filename)
     res = pickled_model.predict([text])
     conf = max(pickled_model.decision_function([text])[0])
-    # if(conf <= 0):
-    #     return 'Unknown'
-    return (res[0], conf)  
+    if(conf <= 0):
+        return 'unknown'
+    return res[0]
